8d1ac4299cd1d8e3d2d1b9fb96c9e805c520129d
Surendra Goud <surendra.goud@oaktreecloud.com>
2024-07-04 18:46:13 +0530

    Step-3 'add' function return input if input is present

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,6 +10,18 @@ describe StringCalculator do
           expect(StringCalculator.add("")).to eq(0)
        end
     end
+
+    context "given '2'" do
+      it "returns 2" do
+        expect(StringCalculator.add("2")).to eql(2)
+      end
+    end
+
+    context "given '7'" do
+      it "returns 7" do
+        expect(StringCalculator.add("7")).to eql(7)
+      end
+    end
   end
 
 end
