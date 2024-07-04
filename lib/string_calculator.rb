ee1a87da37f17f23a870ec7e63410618f92ce263
Surendra Goud <surendra.goud@oaktreecloud.com>
2024-07-04 19:44:23 +0530

    Step-7 add function raise an exception if given input is negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,5 @@
 class StringCalculator
-  def self.add(input)
+  def add(input)
     if input.empty?
       0
     else
@@ -9,8 +9,14 @@ class StringCalculator
         delimiter = parts[0][2..-1]
         input = parts[1]
       end
+      
+      # split based on delimiter
+      numbers = input.split(/#{Regexp.escape(delimiter)}|\n/).map(&:to_i)
 
-      numbers = input.split(/#{Regexp.escape(delimiter)}|\n/).map(&:to_i)    
+      # raise if numbers content negative number
+      negatives = numbers.select { |num| num < 0 }
+      raise "Negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
       numbers.inject(0) { |sum, number| sum + number }
     end
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,28 +5,30 @@ describe StringCalculator do
 
   describe '.add' do
 
+    let(:calculator) { StringCalculator.new }
+
     context "given an empty string" do
        it "return 0" do
-          expect(StringCalculator.add("")).to eq(0)
+          expect(calculator.add("")).to eq(0)
        end
     end
 
     context "given '2'" do
       it "returns 2" do
-        expect(StringCalculator.add("2")).to eql(2)
+        expect(calculator.add("2")).to eql(2)
       end
     end
 
     context "given '7'" do
       it "returns 7" do
-        expect(StringCalculator.add("7")).to eql(7)
+        expect(calculator.add("7")).to eql(7)
       end
     end
     
     context "two numbers" do
       context "given '1,6'" do
         it "returns 7" do
-          expect(StringCalculator.add("1,1,5")).to eql(7)
+          expect(calculator.add("1,1,5")).to eql(7)
         end
       end
     end
@@ -34,7 +36,7 @@ describe StringCalculator do
     context "three numbers" do
       context "given '1\n1,6'" do
         it "returns 8" do
-          expect(StringCalculator.add("1\n1,6")).to eql(8)
+          expect(calculator.add("1\n1,6")).to eql(8)
         end
       end
     end
@@ -42,7 +44,16 @@ describe StringCalculator do
     context "beginning of the string will contain a separate line //[delimiter]\n[numbers…] " do 
       context "given '//;\n1;2'" do
         it "returns 8" do
-          expect(StringCalculator.add("//;\n1;2")).to eql(3)
+          expect(calculator.add("//;\n1;2")).to eql(3)
+        end
+      end
+    end
+
+    context "with nagative numbers" do 
+      context "-1'" do
+        it "raises an exception with the message including the negative numbers" do
+          expect{calculator.add("-1")}.to raise_error("Negatives not allowed: -1")
+          expect{calculator.add("-1,2,-3")}.to raise_error("Negatives not allowed: -1, -3")
         end
       end
     end
