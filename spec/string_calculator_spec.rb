2c4f6c6ae2cbe6bd9f5c3e156767c11661088718
Surendra Goud <surendra.goud@oaktreecloud.com>
2024-07-04 18:41:07 +0530

    Step-2 Add 'add' method and test it

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,13 @@ require "string_calculator"
 
 describe StringCalculator do
 
+  describe '.add' do
+
+    context "given an empty string" do
+       it "return 0" do
+          expect(StringCalculator.add("")).to eq(0)
+       end
+    end
+  end
+
 end
